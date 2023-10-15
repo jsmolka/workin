83727babbdd53a49e0ad86b2691922ba51066038
jsmolka <jsmolka@pm.me>
2023-10-15 16:35:18 +0200

    add heart rate

---
+++ src/bluetooth/heartRateSensor.js
@@ -0,0 +1,31 @@
+export default class HeartRateSensor {
+  constructor() {
+    this.heartRate = null;
+  }
+
+  async connect() {
+    const device = await navigator.bluetooth.requestDevice({
+      filters: [{ services: ["heart_rate"] }],
+    });
+
+    const server = await device.gatt.connect();
+    const service = await server.getPrimaryService("heart_rate");
+    const characteristic = await service.getCharacteristic(
+      "heart_rate_measurement",
+    );
+
+    await characteristic.startNotifications();
+    await characteristic.addEventListener(
+      "characteristicvaluechanged",
+      (event) => {
+        const value = event.target.value;
+        const flags = value.getUint8(0);
+        if (flags & 0x1) {
+          this.heartRate = value.getUint16(1, true);
+        } else {
+          this.heartRate = value.getUint8(1);
+        }
+      },
+    );
+  }
+}
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,23 @@
 <script setup>
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
+import HeartRateSensor from '../bluetooth/heartRateSensor';
 
 defineProps({
   msg: String,
 })
 
-const count = ref(0)
+const sensor = reactive(new HeartRateSensor())
+
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="sensor.connect()">Connect</button>
+    <div v-if="sensor.heartRate != null">Heart rate is {{ sensor.heartRate }}</div>
+    <div v-else>RIP</div>
+
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
