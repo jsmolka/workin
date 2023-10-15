cab778ae7311179ff795cddb79d03f3a7c52c891
jsmolka <jsmolka@pm.me>
2023-10-15 17:35:17 +0200

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,31 @@
 export default {
   content: ['./index.html', './src/**/*.{js,vue}'],
+  theme: {
+    colors: {
+      night: {
+        1: 'var(--night-1)',
+        2: 'var(--night-2)',
+        3: 'var(--night-3)',
+        4: 'var(--night-4)',
+      },
+      snow: {
+        1: 'var(--snow-1)',
+        2: 'var(--snow-2)',
+        3: 'var(--snow-3)',
+      },
+      frost: {
+        1: 'var(--frost-1)',
+        2: 'var(--frost-2)',
+        3: 'var(--frost-3)',
+        4: 'var(--frost-4)',
+      },
+      aurora: {
+        1: 'var(--aurora-1)',
+        2: 'var(--aurora-2)',
+        3: 'var(--aurora-3)',
+        4: 'var(--aurora-4)',
+        5: 'var(--aurora-5)',
+      },
+    },
+  },
 };
