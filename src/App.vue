f5e615ff10cc958af1e719464ef5c9546d438f5c
jsmolka <jsmolka@pm.me>
2023-10-26 14:48:53 +0200

    add wake lock

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,18 @@
     <RouterView />
   </component>
 </template>
+
+<script setup>
+import { useWakeLock } from '@vueuse/core';
+import { onMounted, onUnmounted } from 'vue';
+
+const { request, release } = useWakeLock();
+
+onMounted(async () => {
+  await request();
+});
+
+onUnmounted(async () => {
+  await release();
+});
+</script>
