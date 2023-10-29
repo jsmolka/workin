3d47e7c0d7441f0226010ff6291f5d9a0564100a
jsmolka <jsmolka@pm.me>
2023-10-29 19:10:25 +0100

    support multiple notify args

--- src/utils/notify.js
+++ src/utils/notify.js
@@ -1,6 +1,7 @@
 import { Notyf } from 'notyf';
 import 'notyf/notyf.min.css';
 import './notify.scss';
+import { stringify } from './stringify';
 
 const notyf = new Notyf({
   position: {
@@ -12,12 +13,38 @@ const notyf = new Notyf({
       type: 'info',
       duration: null,
       dismissible: true,
-      background: 'var(--blue-3)',
+      background: '#5e81ac',
+      icon: false,
+    },
+    {
+      type: 'warn',
+      duration: null,
+      dismissible: true,
+      background: '#ebcb8b',
+      icon: false,
+    },
+    {
+      type: 'error',
+      duration: null,
+      dismissible: true,
+      background: '#bf616a',
       icon: false,
     },
   ],
 });
 
-export function notify(message) {
-  return notyf.open({ type: 'info', message });
+function define(type) {
+  return (...args) => {
+    return notyf.open({ type, message: args.map(stringify).join(' ') });
+  };
 }
+
+export const info = define('info');
+export const warn = define('warn');
+export const error = define('error');
+
+export const notify = {
+  info,
+  warn,
+  error,
+};
