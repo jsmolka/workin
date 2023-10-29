7eb382e31a4edb52a3150f5d9a8fab76d1430e6d
jsmolka <jsmolka@pm.me>
2023-10-29 20:03:53 +0100

    refactor

--- src/composables/useInterval.js
+++ src/composables/useInterval.js
@@ -3,8 +3,6 @@ import { interval } from '../utils/interval';
 
 export function useInterval(duration, callback) {
   const stop = interval(duration, callback);
-
   tryOnScopeDispose(stop);
-
-  return { stop };
+  return stop;
 }
