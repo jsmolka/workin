8089d6f8c80db34a537f70cb5924b955c2ccadc2
jsmolka <jsmolka@pm.me>
2023-10-30 09:59:21 +0100

    add signed ints to stream

--- src/modules/bluetooth/dataStream.js
+++ src/modules/bluetooth/dataStream.js
@@ -12,20 +12,41 @@ export class DataStream {
     return uint;
   }
 
+  sint(size) {
+    const sign = 8 * (4 - size);
+    return (this.uint(size) << sign) >> sign;
+  }
+
   u8() {
     return this.uint(1);
+  }
+
+  s8() {
+    return this.sint(1);
   }
 
   u16() {
     return this.uint(2);
   }
 
+  s16() {
+    return this.sint(2);
+  }
+
   u24() {
     return this.uint(3);
   }
 
+  s24() {
+    return this.sint(3);
+  }
+
   u32() {
     return this.uint(4);
+  }
+
+  s32() {
+    return this.sint(4);
   }
 
   get length() {
