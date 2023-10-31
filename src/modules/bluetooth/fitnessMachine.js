4ed8e2b327c2a45563e96e5cfd3c360821f204e5
jsmolka <jsmolka@pm.me>
2023-10-31 21:28:55 +0100

    improve fitness machine

--- src/modules/bluetooth/fitnessMachine.js
+++ src/modules/bluetooth/fitnessMachine.js
@@ -2,6 +2,7 @@ import { bit } from '../../utils/bit';
 import { log } from '../../utils/log';
 import { clamp } from '../../utils/math';
 import { notify } from '../../utils/notify';
+import { deleteNullValues } from '../../utils/objects';
 import { Characteristic } from './characteristic';
 import { DataStream } from './dataStream';
 import { Device } from './device';
@@ -55,6 +56,16 @@ export class FitnessMachine extends Device {
   async initFitnessMachineControlPoint(service) {
     const characteristic = new FitnessMachineControlPoint();
     await characteristic.init(service, 'fitness_machine_control_point');
+    await characteristic.listen((dataView) => {
+      const response = new FitnessMachineControlPointResponse(dataView);
+      if (!response.isOk) {
+        log.error('FTMS control response malformed', response);
+      } else if (!response.isSuccess) {
+        log.error('FTMS control response unsuccessful', response);
+      } else {
+        log.info('FTMS control response', response);
+      }
+    });
     await characteristic.requestControl();
     await characteristic.reset();
     this.control = characteristic;
@@ -65,11 +76,12 @@ export class FitnessMachine extends Device {
     await characteristic.init(service, 'fitness_machine_status');
     await characteristic.listen(async (dataView) => {
       const status = new FitnessMachineStatus(dataView);
-      log.info(status);
-      // if (status.controlPermissionLost) {
-      //   notify.info(`${this.name} control permission lost`);
-      //   await this.disconnect();
-      // }
+      log.info('FTMS status', status);
+      if (status.hasLostControlPermission) {
+        notify.info(`${this.name} control permission lost`);
+        console.log(status);
+        // await this.disconnect();
+      }
     });
   }
 
@@ -87,6 +99,7 @@ export class FitnessMachine extends Device {
       const data = new IndoorBikeData(event.target.value);
       this.power = data.power;
       this.cadence = data.cadence;
+      log.debug(deleteNullValues(data));
     });
     await characteristic.startNotifications();
   }
@@ -103,7 +116,7 @@ export class FitnessMachine extends Device {
   async setPower(value) {
     value = Math.floor(value / this.powerInc) * this.powerInc;
     value = clamp(value, this.powerMin, this.powerMax);
-    await this.control.setPower(value);
+    await this.control.power(value);
     return value;
   }
 
@@ -157,9 +170,9 @@ class FitnessMachineFeature {
       { name: 'strideCount', mask: 1 << 7 },
       { name: 'distance', mask: 1 << 8 },
       { name: 'trainingTime', mask: 1 << 9 },
-      { name: 'heartRateZoneTime2', mask: 1 << 10 },
-      { name: 'heartRateZoneTime3', mask: 1 << 11 },
-      { name: 'heartRateZoneTime5', mask: 1 << 12 },
+      { name: 'timeIn2HeartRateZones', mask: 1 << 10 },
+      { name: 'timeIn3HeartRateZones', mask: 1 << 11 },
+      { name: 'timeIn5HeartRateZones', mask: 1 << 12 },
       { name: 'indoorBikeSimulation', mask: 1 << 13 },
       { name: 'wheelCircumference', mask: 1 << 14 },
       { name: 'spinDown', mask: 1 << 15 },
@@ -176,32 +189,36 @@ class FitnessMachineFeature {
 class IndoorBikeData {
   constructor(dataView) {
     const fields = [
-      { name: 'averageSpeed', mask: 1 << 1, size: 2 },
-      { name: 'cadence', mask: 1 << 2, size: 2 },
-      { name: 'averageCadence', mask: 1 << 3, size: 2 },
-      { name: 'distance', mask: 1 << 4, size: 3 },
-      { name: 'resistance', mask: 1 << 5, size: 2 },
-      { name: 'power', mask: 1 << 6, size: 2 },
-      { name: 'averagePower', mask: 1 << 7, size: 2 },
-      { name: 'expendedEnergy', mask: 1 << 8, size: 2 },
-      { name: 'heartRate', mask: 1 << 9, size: 1 },
-      { name: 'metabolicEquivalent', mask: 1 << 10, size: 1 },
-      { name: 'elapsedTime', mask: 1 << 11, size: 2 },
-      { name: 'remainingTime', mask: 1 << 12, size: 2 },
+      { name: 'averageSpeed', mask: 1 << 1, type: 'u16' },
+      { name: 'cadence', mask: 1 << 2, type: 'u16' },
+      { name: 'averageCadence', mask: 1 << 3, type: 'u16' },
+      { name: 'distance', mask: 1 << 4, type: 'u24' },
+      { name: 'resistance', mask: 1 << 5, type: 's16' },
+      { name: 'power', mask: 1 << 6, type: 's16' },
+      { name: 'averagePower', mask: 1 << 7, type: 's16' },
+      { name: 'expendedEnergy', mask: 1 << 8, type: ['u16', 'u16', 'u8'] },
+      { name: 'heartRate', mask: 1 << 9, type: 'u8' },
+      { name: 'metabolicEquivalent', mask: 1 << 10, type: 'u8' },
+      { name: 'elapsedTime', mask: 1 << 11, type: 'u16' },
+      { name: 'remainingTime', mask: 1 << 12, type: 'u16' },
     ];
 
     const stream = new DataStream(dataView);
-    const flags = stream.u16();
 
+    const flags = stream.u16();
     if ((flags & 0x1) === 0) {
       this.speed = stream.u16();
     } else {
       this.speed = null;
     }
 
-    for (const { name, mask, size } of fields) {
+    for (const { name, mask, type } of fields) {
       if ((flags & mask) !== 0) {
-        this[name] = stream.unsigned(size);
+        if (type instanceof Array) {
+          this[name] = type.map((type) => stream[type]());
+        } else {
+          this[name] = stream[type]();
+        }
       } else {
         this[name] = null;
       }
@@ -213,89 +230,162 @@ class SupportedPowerRange {
   constructor(dataView) {
     const stream = new DataStream(dataView);
 
-    this.min = stream.u16();
-    this.max = stream.u16();
+    this.min = stream.s16();
+    this.max = stream.s16();
     this.inc = stream.u16();
   }
 }
 
-class FitnessMachineControlPoint extends Characteristic {
-  constructor() {
-    super();
+const Parameters = Object.freeze({
+  speed: ['u16'], // 0.01 km/h
+  incline: ['s16'], // 0.1 %
+  resistance: ['u8'], // 0.1
+  power: ['s16'], // 1 W
+  heartRate: ['u8'], // 1 bpm
+  stop: ['u8'], // Opcode
+  expendedEnergy: ['u16'], // 1 calorie
+  stepCount: ['u16'], // 1 step
+  strideCount: ['u16'], // 1 stride
+  distance: ['s24'], // 1 m
+  trainingTime: ['u16'], // 1 s
+  timeIn2HeartRateZones: ['u16', 'u16'], // 1 s
+  timeIn3HeartRateZones: ['u16', 'u16', 'u16'], // 1 s
+  timeIn5HeartRateZones: ['u16', 'u16', 'u16', 'u16', 'u16'], // 1 s
+  indoorBikeSimulation: ['s16', 's16', 'u8', 'u8'], // wind speed 0.001 m/s, grade 0.01 %, rolling resistance 0.0001, wind resistance 0.01 kg/m
+  wheelCircumference: ['u16'], // 0.1 mm
+  spinDown: ['u8'], // Opcode
+  cadence: ['u16'], // 0.5 rpm
+});
 
-    const opcodes = [
-      { code: 0x00, name: 'requestControl', data: [] },
-      { code: 0x01, name: 'reset', data: [] },
-      { code: 0x02, name: 'setSpeed', data: [2] }, // 0.01 km/h
-      { code: 0x03, name: 'setIncline', data: [2] }, // 0.1 %
-      { code: 0x04, name: 'setResistance', data: [1] }, // 0.1
-      { code: 0x05, name: 'setPower', data: [2] }, // 1 W
-      { code: 0x06, name: 'setHeartRate', data: [1] }, // 1 bpm
-      { code: 0x07, name: 'start', data: [] },
-      { code: 0x08, name: 'stop', data: [1] },
-      { code: 0x09, name: 'setExpendedEnergy', data: [2] }, // 1 calorie
-      { code: 0x0a, name: 'setStepCount', data: [2] }, // 1 step
-      { code: 0x0b, name: 'setStrideCount', data: [2] }, // 1 stride
-      { code: 0x0c, name: 'setDistance', data: [3] }, // 1 m
-      { code: 0x0d, name: 'setTrainingTime', data: [2] }, // 1 s
-      { code: 0x0e, name: 'setHeartRateZoneTime2', data: [2, 2] }, // 1 s
-      { code: 0x0f, name: 'setHeartRateZoneTime3', data: [2, 2, 2] }, // 1 s
-      { code: 0x10, name: 'setHeartRateZoneTime5', data: [2, 2, 2, 2, 2] }, // 1 s
-      { code: 0x11, name: 'setIndoorBikeSimulation', data: [2, 2, 1, 1] }, // wind speed 0.001 m/s, grade 0.01 %, rolling resistance 0.0001, wind resistance 0.01 kg/m
-      { code: 0x12, name: 'setWheelCircumference', data: [2] }, // 0.1 mm
-      { code: 0x13, name: 'setSpinDown', data: [1] },
-      { code: 0x14, name: 'setCadence', data: [2] }, // 0.5 rpm
-    ];
+function getParameters(name) {
+  return Parameters[name] ?? [];
+}
 
-    for (const { code, name, data } of opcodes) {
-      this[name] = async (...values) => {
-        const bytes = [];
-        for (const [i, size] of data.entries()) {
-          bytes.push(...bit.bytes(values[i], size));
-        }
-        await this.write(code, ...bytes);
-      };
+function getOpcode(opcodes, opcode) {
+  for (const [name, code] of Object.entries(opcodes)) {
+    if (code === opcode) {
+      return { code, name };
     }
+  }
+  return { code: opcode, name: 'reserved' };
+}
+
+class FitnessMachineControlPoint extends Characteristic {
+  static Opcode = Object.freeze({
+    requestControl: 0x00,
+    reset: 0x01,
+    speed: 0x02,
+    incline: 0x03,
+    resistance: 0x04,
+    power: 0x05,
+    heartRate: 0x06,
+    start: 0x07,
+    stop: 0x08,
+    expendedEnergy: 0x09,
+    stepCount: 0x0a,
+    strideCount: 0x0b,
+    distance: 0x0c,
+    trainingTime: 0x0d,
+    timeIn2HeartRateZones: 0x0e,
+    timeIn3HeartRateZones: 0x0f,
+    timeIn5HeartRateZones: 0x10,
+    indoorBikeSimulation: 0x11,
+    wheelCircumference: 0x12,
+    spinDown: 0x13,
+    cadence: 0x14,
+  });
+}
+
+for (const [name, code] of Object.entries(FitnessMachineControlPoint.Opcode)) {
+  FitnessMachineControlPoint.prototype[name] = async function (...parameters) {
+    const bytes = [];
+    for (const [i, type] of getParameters(name).entries()) {
+      let size;
+      switch (type) {
+        case 'u8':
+        case 's8':
+          size = 1;
+          break;
+        case 'u16':
+        case 's16':
+          size = 2;
+          break;
+        case 'u24':
+        case 's24':
+          size = 3;
+          break;
+        case 'u32':
+        case 's32':
+          size = 4;
+          break;
+      }
+      bytes.push(...bit.bytes(parameters[i], size));
+    }
+    await this.write(code, ...bytes);
+  };
+}
+
+class FitnessMachineControlPointResponse {
+  static Result = Object.freeze({
+    success: 0x01,
+    unsupported: 0x02,
+    parameter: 0x03,
+    error: 0x04,
+    permission: 0x05,
+  });
+
+  constructor(dataView) {
+    const stream = new DataStream(dataView);
+
+    this.response = stream.u8();
+    if (this.isOk) {
+      this.opcode = getOpcode(FitnessMachineControlPoint.Opcode, stream.u8());
+      this.result = getOpcode(FitnessMachineControlPointResponse.Result, stream.u8());
+    } else {
+      this.opcode = getOpcode(FitnessMachineControlPoint.Opcode, -1);
+      this.result = getOpcode(FitnessMachineControlPointResponse.Result, -1);
+    }
+  }
+
+  get isOk() {
+    return this.response === 0x80;
+  }
+
+  get isSuccess() {
+    return this.result.code === FitnessMachineControlPointResponse.Result.success;
   }
 }
 
 class FitnessMachineStatus {
-  constructor(dataView) {
-    const opcodes = [
-      { code: 0x01, name: 'reset', data: [] },
-      { code: 0x02, name: 'stoppedByUser', data: ['u8'] },
-      { code: 0x03, name: 'stoppedByKey', data: [] },
-      { code: 0x04, name: 'startedByUser', data: [] },
-      { code: 0x05, name: 'targetSpeed', data: ['u16'] }, // 0.01 km/h
-      { code: 0x06, name: 'targetIncline', data: ['s16'] }, // 0.1 %
-      { code: 0x07, name: 'targetResistance', data: ['u8'] }, // 0.1
-      { code: 0x08, name: 'targetPower', data: ['s16'] }, // 1 W
-      { code: 0x09, name: 'targetHeartRate', data: ['u8'] }, // 1 bpm
-      { code: 0x0a, name: 'targetExpendedEnergy', data: ['u16'] }, // 1 calorie
-      { code: 0x0b, name: 'targetStepCount', data: ['u16'] }, // 1 step
-      { code: 0x0c, name: 'targetStrideCount', data: ['u16'] }, // 1 stride
-      { code: 0x0d, name: 'targetDistance', data: ['u24'] }, // 1 m
-      { code: 0x0e, name: 'targetTrainingTime', data: ['u16'] }, // 1 s
-      { code: 0x0f, name: 'targetHeartRateZoneTime2', data: ['u16', 'u16'] }, // 1 s
-      { code: 0x10, name: 'targetHeartRateZoneTime3', data: ['u16', 'u16', 'u16'] }, // 1 s
-      { code: 0x11, name: 'targetHeartRateZoneTime5', data: ['u16', 'u16', 'u16', 'u16', 'u16'] }, // 1 s
-      { code: 0x12, name: 'targetIndoorBikeSimulation', data: ['s16', 's16', 'u8', 'u8'] }, // wind speed 0.001 m/s, grade 0.01 %, rolling resistance 0.0001, wind resistance 0.01 kg/m
-      { code: 0x13, name: 'targetWheelCircumference', data: ['u16'] }, // 0.1 mm
-      { code: 0x14, name: 'targetSpinDown', data: ['u8'] },
-      { code: 0x15, name: 'targetCadence', data: ['u16'] }, // 0.5 rpm
-      { code: 0xff, name: 'controlPermissionLost', data: [] },
-    ];
+  static Opcode = Object.freeze({
+    reset: 0x01,
+    stop: 0x02,
+    stopKey: 0x03,
+    start: 0x04,
+    speed: 0x05,
+    incline: 0x06,
+    resistance: 0x07,
+    power: 0x08,
+    heartRate: 0x09,
+    expendedEnergy: 0x0a,
+    stepCount: 0x0b,
+    strideCount: 0x0c,
+    distance: 0x0d,
+    trainingTime: 0x0e,
+    timeIn2HeartRateZones: 0x0f,
+    timeIn3HeartRateZones: 0x10,
+    timeIn5HeartRateZones: 0x11,
+    indoorBikeSimulation: 0x12,
+    wheelCircumference: 0x13,
+    spinDown: 0x14,
+    cadence: 0x15,
+    controlPermissionLost: 0xff,
+  });
 
+  constructor(dataView) {
     const stream = new DataStream(dataView);
 
-    const code = stream.u8();
-    const opcode = opcodes.find((opcode) => opcode.code === code);
-    if (opcode != null) {
-      this.opcode = opcode.name;
-      this.values = opcode.data.map((type) => stream[type]());
-    } else {
-      this.opcode = 'reserved';
-      this.values = [];
-    }
+    this.opcode = getOpcode(FitnessMachineStatus.Opcode, stream.u8());
+    this.parameters = getParameters(this.opcode.name).map((type) => stream[type]());
   }
 }
