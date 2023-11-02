7de8dad995f4f0cb90518bd4cf8a872b6cb0f725
jsmolka <jsmolka@pm.me>
2023-11-02 22:04:28 +0100

    improve workout layout

--- src/views/workouts/Workout.vue
+++ src/views/workouts/Workout.vue
@@ -5,18 +5,35 @@
       :intervals="workout.intervals"
     />
     <div class="flex flex-col justify-between overflow-hidden select-none">
-      <span class="truncate text-gray-1 text-lg font-bold">{{ workout.name }}</span>
-      <span class="truncate">VO2 max</span>
-      <span class="truncate">{{ duration }}</span>
+      <div class="flex flex-col">
+        <span class="truncate text-gray-1 text-lg font-bold">{{ workout.name }}</span>
+        <span class="truncate">{{ workout.zone.name }}</span>
+      </div>
+      <div class="flex gap-4">
+        <div class="flex flex-col">
+          <span>{{ duration }}</span>
+          <span class="unit">Duration</span>
+        </div>
+        <div class="flex flex-col">
+          <span>{{ calories }}</span>
+          <span class="unit">cal</span>
+        </div>
+        <div class="flex flex-col">
+          <span>{{ maxPower }}</span>
+          <span class="unit">Max W</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { storeToRefs } from 'pinia';
 import { computed } from 'vue';
 import Intervals from '../../components/Intervals.vue';
 import { Time } from '../../modules/time';
 import { Workout } from '../../modules/workout';
+import { useAthleteStore } from '../../stores/athlete';
 
 const props = defineProps({
   workout: {
@@ -25,5 +42,19 @@ const props = defineProps({
   },
 });
 
+const { athlete } = storeToRefs(useAthleteStore());
+
 const duration = computed(() => new Time(0, 0, props.workout.duration).format('HH:mm:ss'));
+const calories = computed(() => Math.round(props.workout.calories(athlete.value.ftp)));
+const maxPower = computed(() => Math.round(props.workout.maxIntensity * athlete.value.ftp));
 </script>
+
+<style lang="scss" scoped>
+.unit {
+  @apply text-gray-3;
+  @apply text-xs;
+  @apply font-medium;
+  @apply uppercase;
+  @apply whitespace-nowrap;
+}
+</style>
