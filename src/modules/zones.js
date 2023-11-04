fd49b25b4feb8d75706a724727d2bb24030e1c06
jsmolka <jsmolka@pm.me>
2023-11-04 12:53:30 +0100

    finish workouts

--- src/modules/zones.js
+++ src/modules/zones.js
@@ -1,10 +1,9 @@
-// https://www.highnorth.co.uk/articles/cycling-training-zones
 export const zones = [
   { name: 'Active recovery', min: Number.MIN_SAFE_INTEGER, max: 0.55 },
   { name: 'Endurance', min: 0.55, max: 0.75 },
   { name: 'Tempo', min: 0.75, max: 0.9 },
   { name: 'Threshold', min: 0.9, max: 1.05 },
   { name: 'VO2 max', min: 1.05, max: 1.2 },
-  { name: 'Anaerobic', min: 1.2, max: 1.3 },
-  { name: 'Neuromuscular', min: 1.3, max: Number.MAX_SAFE_INTEGER },
+  { name: 'Anaerobic', min: 1.2, max: 1.5 },
+  { name: 'Neuromuscular', min: 1.5, max: Number.MAX_SAFE_INTEGER },
 ];
--- src/stores/data/workouts.js
+++ src/stores/data/workouts.js
@@ -1,15 +1,15 @@
 import { Interval } from '../../modules/interval';
 import { Workout } from '../../modules/workout';
 
-function range(start, stop, step, callback) {
+function range(start, stop, step, fn) {
   const data = [];
   for (let i = start; i <= stop; i += step) {
-    data.push(callback(i));
+    data.push(fn(i));
   }
   return data;
 }
 
-function w(name, data) {
+function workout(name, data) {
   const intervals = [];
   for (const { duration, intensity } of data.flat()) {
     const previous = intervals.at(-1);
@@ -19,23 +19,23 @@ function w(name, data) {
       intervals.push(new Interval(duration, intensity));
     }
   }
-  return new Workout(name, data.flat());
+  return new Workout(name, intervals);
 }
 
-function i(minutes, intensity) {
-  return new Interval(60 * minutes, intensity);
+function interval(minutes, intensity) {
+  return new Interval(minutes * 60, intensity);
 }
 
-function s(minutes, from, to, count) {
+function ramp(minutes, from, to, count) {
   const result = [];
   const offset = (to - from) / (count - 1);
-  for (let index = 0; index < count; index++) {
-    result.push(i(minutes / count, from + index * offset));
+  for (let i = 0; i < count; i++) {
+    result.push(interval(minutes / count, from + i * offset));
   }
   return result;
 }
 
-function r(count, data, pop = true) {
+function repeat(count, data, pop = true) {
   const result = [];
   while (count--) {
     result.push(...data.flat());
@@ -49,144 +49,136 @@ function r(count, data, pop = true) {
 // prettier-ignore
 export const workouts = [
   // Active recovery
-
-  ...range(30, 60, 30, (n) =>
-    w('Recovery', [
-      i(n, 0.5),
+  ...range(30, 60, 30, (i) =>
+    workout('Recovery', [
+      interval(i, 0.5),
     ]),
   ),
 
   // Endurance
-
-  ...range(45, 135, 30, (n) =>
-    w('LIT', [
-      s(10, 0.5, 0.6, 4),
-      i(n, 0.6),
-      s(5, 0.5, 0.4, 4),
+  ...range(45, 135, 30, (i) =>
+    workout('LIT', [
+      ramp(10, 0.5, 0.6, 4),
+      interval(i, 0.6),
+      ramp(5, 0.5, 0.4, 4),
     ]),
   ),
 
-  ...range(45, 135, 30, (n) =>
-    w('Zone 2', [
-      s(10, 0.5, 0.7, 4),
-      i(n, 0.70),
-      s(5, 0.5, 0.4, 4),
+  ...range(45, 135, 30, (i) =>
+    workout('Zone 2', [
+      ramp(10, 0.5, 0.7, 4),
+      interval(i, 0.7),
+      ramp(5, 0.5, 0.4, 4),
     ]),
   ),
 
   // Threshold
-  // https://www.youtube.com/watch?v=MWaMVhHo-zE
-
-  ...range(3, 4, 1, (n) =>
-    w(`${n} x 10`, [
-      s(10, 0.5, 1.0, 4),
-      i(5, 0.5),
-      r(n, [
-        i(10, 1.0),
-        i(5, 0.5)
+  ...range(3, 4, 1, (i) =>
+    workout(`${i} x 10`, [
+      ramp(10, 0.5, 1.0, 4),
+      interval(5, 0.5),
+      repeat(i, [
+        interval(10, 1.0),
+        interval(5, 0.5),
       ]),
-      s(5, 0.5, 0.4, 4),
+      ramp(5, 0.5, 0.4, 4),
     ]),
   ),
 
-  ...range(3, 4, 1, (n) =>
-    w(`${n} x 15`, [
-      s(10, 0.5, 1.0, 4),
-      i(5, 0.5),
-      r(n, [
-        i(15, 1.0),
-        i(7.5, 0.5)
+  ...range(3, 4, 1, (i) =>
+    workout(`${i} x 15`, [
+      ramp(10, 0.5, 1.0, 4),
+      interval(5, 0.5),
+      repeat(i, [
+        interval(15, 1.0),
+        interval(7.5, 0.5),
       ]),
-      s(5, 0.5, 0.4, 4),
+      ramp(5, 0.5, 0.4, 4),
     ]),
   ),
 
-  ...range(2, 3, 1, (n) =>
-    w(`${n} x 20`, [
-      s(10, 0.5, 1.0, 4),
-      i(5, 0.5),
-      r(n, [
-        i(20, 1.0),
-        i(10, 0.5)
+  ...range(2, 3, 1, (i) =>
+    workout(`${i} x 20`, [
+      ramp(10, 0.5, 1.0, 4),
+      interval(5, 0.5),
+      repeat(i, [
+        interval(20, 1.0),
+        interval(10, 0.5),
       ]),
-      s(5, 0.5, 0.4, 4),
+      ramp(5, 0.5, 0.4, 4),
     ]),
   ),
 
   // Lactate clearance
-
-  ...range(2, 6, 2, (n) =>
-    w(`${n} x 10 LC`, [
-      s(10, 0.5, 1.0, 4),
-      i(5, 0.5),
-      r(n, [
-        r(5, [
-          i(1, 0.9),
-          i(1, 1.1),
+  ...range(2, 4, 1, (i) =>
+    workout(`${i} x 4 x 2/2`, [
+      ramp(10, 0.5, 1.0, 4),
+      interval(5, 0.5),
+      repeat(i, [
+        repeat(4, [
+          interval(2, 1.05),
+          interval(2, 0.9),
         ], false),
-        i(5, 0.5),
+        interval(4, 0.5),
       ]),
-      s(5, 0.5, 0.4, 4),
+      ramp(5, 0.5, 0.4, 4),
     ]),
   ),
 
   // VO2 max
-  // https://www.youtube.com/watch?v=YBgAr7kLsZY
-
-  ...range(2, 6, 2, (n) =>
-    w(`${n} x 4`, [
-      s(10, 0.5, 1.0, 4),
-      i(5, 0.5),
-      r(n, [
-        i(4, 1.15),
-        i(4, 0.5),
+  ...range(4, 6, 1, (i) =>
+    workout(`${i} x 8`, [
+      ramp(10, 0.5, 1.0, 4),
+      interval(5, 0.5),
+      repeat(i, [
+        interval(8, 1.05),
+        interval(4, 0.5),
       ]),
-      s(5, 0.5, 0.4, 4),
+      ramp(5, 0.5, 0.4, 4),
     ]),
   ),
 
-  ...range(2, 6, 2, (n) =>
-    w(`${n} x 8`, [
-      s(10, 0.5, 1.0, 4),
-      i(5, 0.5),
-      r(n, [
-        i(8, 1.05),
-        i(4, 0.5),
+  ...range(4, 6, 1, (i) =>
+    workout(`${i} x 4`, [
+      ramp(10, 0.5, 1.0, 4),
+      interval(5, 0.5),
+      repeat(i, [
+        interval(4, 1.15),
+        interval(4, 0.5),
       ]),
-      s(5, 0.5, 0.4, 4),
+      ramp(5, 0.5, 0.4, 4),
     ]),
   ),
+
 
   // Anaerobic
-  // https://www.youtube.com/watch?v=YBgAr7kLsZY
-
-  ...range(1, 3, 1, (n) =>
-    w(`${n} x 10 x 30`, [
-      s(10, 0.5, 1.0, 4),
-      i(5, 0.5),
-      r(n, [
-        r(10, [
-          i(0.5, 1.25),
-          i(0.5, 0.5),
+  ...range(2, 3, 1, (i) =>
+    workout(`${i} x 10 x 30/30`, [
+      ramp(10, 0.5, 1.0, 4),
+      interval(5, 0.5),
+      repeat(i, [
+        repeat(10, [
+          interval(0.5, 1.25),
+          interval(0.5, 0.5),
         ]),
-        i(5, 0.5),
+        interval(5, 0.5),
       ]),
-      s(5, 0.5, 0.4, 4),
+      ramp(5, 0.5, 0.4, 4),
     ]),
   ),
 
-  ...range(1, 3, 1, (n) =>
-    w(`${n} x 20 x 30`, [
-      s(10, 0.5, 1.0, 4),
-      i(5, 0.5),
-      r(n, [
-        r(20, [
-          i(0.5, 1.25),
-          i(0.5, 0.5),
+  ...range(2, 3, 1, (i) =>
+    workout(`${i} x 20 x 30/30`, [
+      ramp(10, 0.5, 1.0, 4),
+      interval(5, 0.5),
+      repeat(i, [
+        repeat(20, [
+          interval(0.5, 1.25),
+          interval(0.5, 0.5),
         ]),
-        i(10, 0.5),
+        interval(10, 0.5),
       ]),
-      s(5, 0.5, 0.4, 4),
+      ramp(5, 0.5, 0.4, 4),
     ]),
-  )
+  ),
 ];
