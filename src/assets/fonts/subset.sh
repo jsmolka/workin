f6b5db9e916fdf8ae358d20c4e40f89a9a536be1
jsmolka <jsmolka@pm.me>
2023-11-08 19:57:38 +0100

    remove cv07

--- src/assets/fonts/subset.sh
+++ src/assets/fonts/subset.sh
@@ -3,4 +3,4 @@
 # https://jrgraphix.net/r/Unicode/
 # U+0000-007F  Latin
 # U+0080-00FF  Latin-1 Supplement
-pyftsubset Inter.ttf --unicodes="U+0000-00FF" --layout-features="calt,kern,cv07,tnum" --flavor="woff2" --output-file="inter.woff2"
+pyftsubset Inter.ttf --unicodes="U+0000-00FF" --layout-features="calt,kern,tnum" --flavor="woff2" --output-file="inter.woff2"
