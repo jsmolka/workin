80340466fd9958fe468888cc62db4704ae55cdd9
jsmolka <jsmolka@pm.me>
2023-11-09 18:30:07 +0100

    add preserve component

---
+++ src/components/Preserve.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="preserve" :data-preserve="text">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.preserve::after {
+  content: attr(data-preserve);
+  @apply block;
+  @apply h-0;
+  @apply overflow-hidden;
+  @apply invisible;
+}
+</style>
