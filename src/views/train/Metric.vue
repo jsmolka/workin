d4668e7df2472a8b57f2c1b6fe2ecf9fb2ca8618
jsmolka <jsmolka@pm.me>
2023-11-11 10:00:40 +0100

    round metrics

--- src/views/train/Metric.vue
+++ src/views/train/Metric.vue
@@ -2,17 +2,32 @@
   <div class="grid place-items-center gap-2">
     <span class="text-gray-3 text-xs font-medium uppercase">{{ text }}</span>
     <span class="text-4xl font-bold" :class="value != null ? 'text-gray-1' : 'text-gray-3'">
-      {{ value ?? '-' }}
+      {{ valueText }}
     </span>
   </div>
 </template>
 
 <script setup>
-defineProps({
+import { computed } from 'vue';
+
+const props = defineProps({
   text: {
     type: String,
     required: true,
   },
-  value: null,
+  value: {
+    default: null,
+  },
+});
+
+const valueText = computed(() => {
+  const value = props.value;
+  if (value == null) {
+    return '-';
+  }
+  if (typeof value === 'number') {
+    return Math.round(value);
+  }
+  return value;
 });
 </script>
