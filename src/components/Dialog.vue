dba073e22ed32d1443b52b773f64f5b998fe8234
jsmolka <jsmolka@pm.me>
2023-11-11 12:21:16 +0100

    add dialog

---
+++ src/components/Dialog.vue
@@ -0,0 +1,22 @@
+<template>
+  <Dialog :open="open" @close="close">
+    <div class="fixed inset-0 flex justify-center items-center p-4 bg-black/25">
+      <DialogPanel class="max-w-screen-sm p-4 bg-gray-7 rounded-sm">
+        <slot :close="close" />
+      </DialogPanel>
+    </div>
+  </Dialog>
+</template>
+
+<script setup>
+import { Dialog, DialogPanel } from '@headlessui/vue';
+
+const open = defineModel('open', {
+  type: Boolean,
+  required: true,
+});
+
+const close = () => {
+  open.value = false;
+};
+</script>
