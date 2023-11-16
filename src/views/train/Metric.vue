db830aef3883985e537e98f06efb0de010091f0c
jsmolka <jsmolka@pm.me>
2023-11-16 19:22:53 +0100

    refactor

--- src/views/train/Metric.vue
+++ src/views/train/Metric.vue
@@ -2,7 +2,7 @@
   <div class="grid place-items-center gap-2">
     <span class="text-gray-3 text-xs font-medium uppercase">{{ text }}</span>
     <span class="text-4xl font-bold" :class="value != null ? 'text-gray-1' : 'text-gray-3'">
-      {{ valueText }}
+      {{ metric }}
     </span>
   </div>
 </template>
@@ -20,7 +20,7 @@ const props = defineProps({
   },
 });
 
-const valueText = computed(() => {
+const metric = computed(() => {
   const value = props.value;
   if (value == null) {
     return '-';
