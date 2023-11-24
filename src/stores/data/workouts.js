5cc17b63ff4c06a87a32e6548ad8e23ff687dae1
jsmolka <jsmolka@pm.me>
2023-11-24 22:56:05 +0100

    add warmup to easier workouts

--- src/stores/data/workouts.js
+++ src/stores/data/workouts.js
@@ -1,4 +1,4 @@
-import { Interval, cooldown, warmup } from '../../modules/interval';
+import { Interval, cooldown, ramp, warmup } from '../../modules/interval';
 import { Workout } from '../../modules/workout';
 import { parseSeconds } from '../../utils/datetime';
 
@@ -35,178 +35,181 @@ function repeat(count, data, pop = true) {
 // prettier-ignore
 export const workouts = [
   // Active recovery
-  ...['30:00', '1:00:00'].map((d) =>
+  ...['24:00', '54:00'].map((duration) =>
     workout('Recovery', [
-      interval(d, 0.5),
+      ramp(parseSeconds('06:00'), 0.4, 0.5, 6),
+      interval(duration, 0.5),
     ]),
   ),
 
   // Endurance
-  ...['1:00:00', '1:30:00', '2:00:00', '2:30:00'].map((d) =>
+  ...['54:00', '1:24:00', '1:54:00', '2:24:00'].map((duration) =>
     workout('Endurance', [
-      interval(d, 0.6),
+      ramp(parseSeconds('06:00'), 0.4, 0.6, 6),
+      interval(duration, 0.6),
     ]),
   ),
 
   // Zone 2
-  ...['1:00:00', '1:30:00', '2:00:00', '2:30:00'].map((d) =>
+  ...['54:00', '1:24:00', '1:54:00', '2:24:00'].map((duration) =>
     workout('Zone 2', [
-      interval(d, 0.7),
+      ramp(parseSeconds('06:00'), 0.4, 0.7, 6),
+      interval(duration, 0.7),
     ]),
   ),
 
   // Threshold
-  ...[3, 4].map((r) =>
-    workout(`${r} x 10`, [
-      ...warmup,
-      repeat(r, [
+  ...[3, 4].map((repeats) =>
+    workout(`${repeats} x 10`, [
+      warmup,
+      repeat(repeats, [
         interval('10:00', 1.0),
         interval('05:00', 0.5),
       ]),
-      ...cooldown,
+      cooldown,
     ]),
   ),
 
-  ...[3, 4].map((r) =>
-    workout(`${r} x 15`, [
-      ...warmup,
-      repeat(r, [
+  ...[3, 4].map((repeats) =>
+    workout(`${repeats} x 15`, [
+      warmup,
+      repeat(repeats, [
         interval('15:00', 1.0),
         interval('07:30', 0.5),
       ]),
-      ...cooldown,
+      cooldown,
     ]),
   ),
 
-  ...[2, 3].map((r) =>
-    workout(`${r} x 20`, [
-      ...warmup,
-      repeat(r, [
+  ...[2, 3].map((repeats) =>
+    workout(`${repeats} x 20`, [
+      warmup,
+      repeat(repeats, [
         interval('20:00', 1.0),
         interval('10:00', 0.5),
       ]),
-      ...cooldown,
+      cooldown,
     ]),
   ),
 
   // Lactate clearance
-  ...[2, 3, 4].map((r) =>
-    workout(`${r} x 4 x 2/2`, [
-      ...warmup,
-      repeat(r, [
+  ...[2, 3, 4].map((repeats) =>
+    workout(`${repeats} x 4 x 2/2`, [
+      warmup,
+      repeat(repeats, [
         repeat(4, [
           interval('02:00', 1.05),
           interval('02:00', 0.9),
         ], false),
         interval('08:00', 0.5),
       ]),
-      ...cooldown,
+      cooldown,
     ]),
   ),
 
-  ...[2, 3, 4].map((r) =>
-    workout(`${r} x 3 x 1/4`, [
-      ...warmup,
-      repeat(r, [
+  ...[2, 3, 4].map((repeats) =>
+    workout(`${repeats} x 3 x 1/4`, [
+      warmup,
+      repeat(repeats, [
         repeat(3, [
           interval('01:00', 1.15),
           interval('04:00', 0.85),
         ], false),
         interval('07:30', 0.5),
       ]),
-      ...cooldown,
+      cooldown,
     ]),
   ),
 
   // VO2 max
-  ...[4, 5, 6].map((r) =>
-    workout(`${r} x 4`, [
-      ...warmup,
-      repeat(r, [
+  ...[4, 5, 6].map((repeats) =>
+    workout(`${repeats} x 4`, [
+      warmup,
+      repeat(repeats, [
         interval('04:00', 1.15),
         interval('04:00', 0.5),
       ]),
-      ...cooldown,
+      cooldown,
     ]),
   ),
 
-  ...[4, 5, 6].map((r) =>
-    workout(`${r} x 8`, [
-      ...warmup,
-      repeat(r, [
+  ...[4, 5, 6].map((repeats) =>
+    workout(`${repeats} x 8`, [
+      warmup,
+      repeat(repeats, [
         interval('08:00', 1.05),
         interval('08:00', 0.5),
       ]),
-      ...cooldown
+      cooldown
     ]),
   ),
 
-  ...[4, 5, 6].map((r) =>
-    workout(`${r} x 1/4`, [
-      ...warmup,
-      repeat(r, [
+  ...[4, 5, 6].map((repeats) =>
+    workout(`${repeats} x 1/4`, [
+      warmup,
+      repeat(repeats, [
         interval('01:00', 1.25),
         interval('04:00', 1.05),
         interval('05:00', 0.5),
       ]),
-      ...cooldown,
+      cooldown,
     ]),
   ),
 
   // Anaerobic
-  ...[2, 3].map((r) =>
-    workout(`${r} x 10 x 30/30`, [
-      ...warmup,
-      repeat(r, [
+  ...[2, 3].map((repeats) =>
+    workout(`${repeats} x 10 x 30/30`, [
+      warmup,
+      repeat(repeats, [
         repeat(10, [
           interval('00:30', 1.25),
           interval('00:30', 0.5),
         ]),
         interval('05:00', 0.5),
       ]),
-      ...cooldown,
+      cooldown,
     ]),
   ),
 
-  ...[2, 3].map((r) =>
-    workout(`${r} x 20 x 30/30`, [
-      ...warmup,
-      repeat(r, [
+  ...[2, 3].map((repeats) =>
+    workout(`${repeats} x 20 x 30/30`, [
+      warmup,
+      repeat(repeats, [
         repeat(20, [
           interval('00:30', 1.25),
           interval('00:30', 0.5),
         ]),
         interval('05:00', 0.5),
       ]),
-      ...cooldown,
+      cooldown,
     ]),
   ),
 
-  ...[2, 3].map((r) =>
-    workout(`${r} x 10 x 40/20`, [
-      ...warmup,
-      repeat(r, [
+  ...[2, 3].map((repeats) =>
+    workout(`${repeats} x 10 x 40/20`, [
+      warmup,
+      repeat(repeats, [
         repeat(10, [
           interval('00:40', 1.25),
           interval('00:20', 0.5),
         ]),
         interval('05:00', 0.5),
       ]),
-      ...cooldown,
+      cooldown,
     ]),
   ),
 
-  ...[2, 3].map((r) =>
-    workout(`${r} x 20 x 40/20`, [
-      ...warmup,
-      repeat(r, [
+  ...[2, 3].map((repeats) =>
+    workout(`${repeats} x 20 x 40/20`, [
+      warmup,
+      repeat(repeats, [
         repeat(20, [
           interval('00:40', 1.25),
           interval('00:20', 0.5),
         ]),
         interval('05:00', 0.5),
       ]),
-      ...cooldown,
+      cooldown,
     ]),
   ),
 ];
