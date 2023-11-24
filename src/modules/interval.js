1ed06218645410668f73c8a94a927aef415bf2c6
jsmolka <jsmolka@pm.me>
2023-11-24 22:24:28 +0100

    improve warmup

--- src/modules/interval.js
+++ src/modules/interval.js
@@ -12,19 +12,14 @@ createSchema(Interval, {
   intensity: primitive(),
 });
 
-export const warmup = [
-  new Interval(120, 0.5),
-  new Interval(120, 0.625),
-  new Interval(120, 0.75),
-  new Interval(120, 0.875),
-  new Interval(120, 1.0),
-  new Interval(300, 0.5),
-];
+export function ramp(seconds, from, to, steps) {
+  const step = (to - from) / (steps - 1);
+  const intervals = [];
+  for (let i = 0; i < steps; i++) {
+    intervals.push(new Interval(seconds / steps, from + i * step));
+  }
+  return intervals;
+}
 
-export const cooldown = [
-  new Interval(60, 0.5),
-  new Interval(60, 0.475),
-  new Interval(60, 0.45),
-  new Interval(60, 0.425),
-  new Interval(60, 0.4),
-];
+export const warmup = [...ramp(600, 0.4, 1.0, 10), new Interval(300, 0.5)];
+export const cooldown = ramp(300, 0.5, 0.4, 5);
