5945d53a87e292056f96ceec94c5501e6393a0a6
jsmolka <jsmolka@pm.me>
2023-11-26 15:02:19 +0100

    improve workouts

--- src/stores/data/workouts.js
+++ src/stores/data/workouts.js
@@ -30,35 +30,35 @@ function repeat(count, data, pop = true) {
   return result;
 }
 
+function map(intensity) {
+  return 1.3 * intensity;
+}
+
 // https://robertovukovic.com/cycling-interval-training/
 
 // prettier-ignore
 export const workouts = [
-  // Active recovery
   ...['24:00', '54:00'].map((duration) =>
     workout('Recovery', [
-      ramp(parseSeconds('06:00'), 0.4, 0.5, 6),
+      ramp(parseSeconds('6:00'), 0.4, 0.5, 6),
       interval(duration, 0.5),
     ]),
   ),
 
-  // Endurance
-  ...['54:00', '1:24:00', '1:54:00', '2:24:00'].map((duration) =>
+  ...['54:00', '1:24:00', '1:54:00', '2:24:00', '2:54:00'].map((duration) =>
     workout('Endurance', [
-      ramp(parseSeconds('06:00'), 0.4, 0.6, 6),
+      ramp(parseSeconds('6:00'), 0.4, 0.6, 6),
       interval(duration, 0.6),
     ]),
   ),
 
-  // Zone 2
-  ...['54:00', '1:24:00', '1:54:00', '2:24:00'].map((duration) =>
+  ...['54:00', '1:24:00', '1:54:00', '2:24:00', '2:54:00'].map((duration) =>
     workout('Zone 2', [
-      ramp(parseSeconds('06:00'), 0.4, 0.7, 6),
+      ramp(parseSeconds('6:00'), 0.4, 0.7, 6),
       interval(duration, 0.7),
     ]),
   ),
 
-  // Threshold
   ...[3, 4].map((repeats) =>
     workout(`${repeats} x 10`, [
       warmup,
@@ -92,122 +92,106 @@ export const workouts = [
     ]),
   ),
 
-  // Lactate clearance
-  ...[2, 3, 4].map((repeats) =>
-    workout(`${repeats} x 4 x 2/2`, [
-      warmup,
-      repeat(repeats, [
-        repeat(4, [
-          interval('02:00', 1.05),
-          interval('02:00', 0.9),
-        ], false),
-        interval('08:00', 0.5),
-      ]),
-      cooldown,
-    ]),
-  ),
-
-  ...[2, 3, 4].map((repeats) =>
-    workout(`${repeats} x 3 x 1/4`, [
+  ...[3, 4, 5].map((repeats) =>
+    workout(`${repeats} x 12 Lactate`, [
       warmup,
       repeat(repeats, [
         repeat(3, [
-          interval('01:00', 1.15),
-          interval('04:00', 0.85),
+          interval('2:00', 1.05),
+          interval('2:00', 0.85),
         ], false),
-        interval('07:30', 0.5),
+        interval('6:00', 0.5),
       ]),
       cooldown,
     ]),
   ),
 
-  // VO2 max
   ...[4, 5, 6].map((repeats) =>
     workout(`${repeats} x 4`, [
       warmup,
       repeat(repeats, [
-        interval('04:00', 1.15),
-        interval('04:00', 0.5),
+        interval('4:00', 1.15),
+        interval('4:00', 0.5),
       ]),
       cooldown,
     ]),
   ),
 
+  // Seiler
+  // https://pubmed.ncbi.nlm.nih.gov/21812820/
+  // https://www.researchgate.net/publication/51543724/
   ...[4, 5, 6].map((repeats) =>
     workout(`${repeats} x 8`, [
       warmup,
       repeat(repeats, [
-        interval('08:00', 1.05),
-        interval('08:00', 0.5),
+        interval('8:00', 1.05),
+        interval('4:00', 0.5),
       ]),
       cooldown
     ]),
   ),
 
+  // Rønnestad
+  // https://pubmed.ncbi.nlm.nih.gov/31621643/
+  // https://www.researchgate.net/publication/356469064/
   ...[4, 5, 6].map((repeats) =>
-    workout(`${repeats} x 1/4`, [
+    workout(`${repeats} x 5 Decrease`, [
       warmup,
       repeat(repeats, [
-        interval('01:00', 1.25),
-        interval('04:00', 1.05),
-        interval('05:00', 0.5),
+        interval('1:30', map(1.0)),
+        interval('3:30', map(0.85)),
+        interval('5:00', 0.5),
       ]),
       cooldown,
     ]),
   ),
 
-  // Anaerobic
-  ...[2, 3].map((repeats) =>
-    workout(`${repeats} x 10 x 30/30`, [
+  // Bossi
+  // https://pubmed.ncbi.nlm.nih.gov/32244222/
+  // https://www.researchgate.net/publication/336641479/
+  // https://hiitscience.com/hiit-power-variation/
+  ...[4, 5, 6].map((repeats) =>
+    workout(`${repeats} x 5 Variation`, [
       warmup,
       repeat(repeats, [
-        repeat(10, [
-          interval('00:30', 1.25),
-          interval('00:30', 0.5),
-        ]),
-        interval('05:00', 0.5),
+        interval('0:30', map(1.0)),
+        interval('1:00', map(0.77)),
+        interval('0:30', map(1.0)),
+        interval('1:00', map(0.77)),
+        interval('0:30', map(1.0)),
+        interval('1:30', map(0.77)),
+        interval('5:00', 0.5),
       ]),
       cooldown,
     ]),
   ),
 
-  ...[2, 3].map((repeats) =>
-    workout(`${repeats} x 20 x 30/30`, [
+  // Rønnestad
+  // https://pubmed.ncbi.nlm.nih.gov/24382021/
+  // https://www.researchgate.net/publication/338808139/
+  ...[3, 4].map((repeats) =>
+    workout(`${repeats} x 13 x 30/15`, [
       warmup,
       repeat(repeats, [
-        repeat(20, [
-          interval('00:30', 1.25),
-          interval('00:30', 0.5),
+        repeat(13, [
+          interval('0:30', map(1.0)),
+          interval('0:15', map(0.5)),
         ]),
-        interval('05:00', 0.5),
+        interval('5:00', 0.5),
       ]),
       cooldown,
     ]),
   ),
 
-  ...[2, 3].map((repeats) =>
-    workout(`${repeats} x 10 x 40/20`, [
+  ...[3, 4].map((repeats) =>
+    workout(`${repeats} x 15 x 40/20`, [
       warmup,
       repeat(repeats, [
-        repeat(10, [
-          interval('00:40', 1.25),
-          interval('00:20', 0.5),
+        repeat(15, [
+          interval('0:40', map(1.0)),
+          interval('0:20', map(0.5)),
         ]),
-        interval('05:00', 0.5),
-      ]),
-      cooldown,
-    ]),
-  ),
-
-  ...[2, 3].map((repeats) =>
-    workout(`${repeats} x 20 x 40/20`, [
-      warmup,
-      repeat(repeats, [
-        repeat(20, [
-          interval('00:40', 1.25),
-          interval('00:20', 0.5),
-        ]),
-        interval('05:00', 0.5),
+        interval('7:30', 0.5),
       ]),
       cooldown,
     ]),
