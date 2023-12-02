e8ed1ab1363eb92dab207901b5bac8d2e8eea32e
jsmolka <jsmolka@pm.me>
2023-12-02 12:26:44 +0100

    remember scroll position on reload

--- src/router/scroller.js
+++ src/router/scroller.js
@@ -1,7 +1,6 @@
 // Based on https://github.com/antfu/vue-router-better-scroller
 
 import { nextTick } from 'vue';
-import { isNavigationFailure } from 'vue-router';
 
 export function setupRouterScroller(router, selectors) {
   if (router.options.scrollBehavior) {
@@ -12,28 +11,28 @@ export function setupRouterScroller(router, selectors) {
 
   router.options.scrollBehavior = () => {};
 
+  window.addEventListener('beforeunload', () => {
+    capturePositions(selectors);
+  });
+
   router.beforeResolve((to, from) => {
     if (history.state?.current === to.fullPath) {
       return;
     }
-
-    const positions = capturePositions(selectors);
-    history.replaceState({ ...history.state, vueRouterPositions: positions }, '');
+    capturePositions(selectors);
   });
 
-  router.afterEach((to, from, failure) => {
-    if (isNavigationFailure(failure)) {
+  router.afterEach(async (to, from, failure) => {
+    if (failure) {
       return;
     }
-
-    const positions = history.state.vueRouterPositions;
-    nextTick(() => {
-      applyPositions(positions, selectors);
-    });
+    const positions = history.state.positions;
+    await nextTick();
+    restorePositions(selectors, positions);
   });
 }
 
-export function createRouterScroller(options) {
+export function createRouterScroller(selectors) {
   return {
     install(app) {
       const router = app.config.globalProperties.$router;
@@ -42,18 +41,9 @@ export function createRouterScroller(options) {
           'Router instance is not found on this Vue app. This plugin should be installed after Vue Router.',
         );
       }
-      setupRouterScroller(router, options);
+      setupRouterScroller(router, selectors);
     },
   };
-}
-
-function capturePositions(selectors) {
-  const positions = {};
-  for (const selector of selectors) {
-    const elements = querySelectorAll(selector);
-    positions[selector] = [...elements].map((element) => getScrollPosition(element));
-  }
-  return positions;
 }
 
 function querySelectorAll(selector) {
@@ -66,13 +56,20 @@ function querySelectorAll(selector) {
   return document.querySelectorAll(selector);
 }
 
-function getScrollPosition(element) {
-  return element instanceof Window
-    ? { left: window.scrollX, top: window.scrollY }
-    : { left: element.scrollLeft, top: element.scrollTop };
+function capturePositions(selectors) {
+  const positions = {};
+  for (const selector of selectors) {
+    const elements = querySelectorAll(selector);
+    positions[selector] = [...elements].map((element) =>
+      element instanceof Window
+        ? { top: window.scrollY, left: window.scrollX }
+        : { top: element.scrollTop, left: element.scrollLeft },
+    );
+  }
+  history.replaceState({ ...history.state, positions }, '');
 }
 
-function applyPositions(positions, selectors) {
+function restorePositions(selectors, positions) {
   for (const selector of selectors) {
     const elements = querySelectorAll(selector);
     for (const [i, element] of elements.entries()) {
