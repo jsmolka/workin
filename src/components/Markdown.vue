a83dcff98b47de38902edef17c2f61f5ff981a28
jsmolka <jsmolka@pm.me>
2024-01-09 22:26:25 +0100

    use micromark to render markdown

---
+++ src/components/Markdown.vue
@@ -0,0 +1,46 @@
+<template>
+  <span class="markdown" v-html="html" />
+</template>
+
+<script setup>
+import { micromark } from 'micromark';
+import { computed } from 'vue';
+
+const props = defineProps({
+  content: {
+    type: String,
+    required: true,
+  },
+});
+
+const html = computed(() => micromark(props.content));
+</script>
+
+<style lang="scss">
+.markdown {
+  @apply leading-relaxed;
+
+  & > *:not(:first-child) {
+    @apply mt-4;
+  }
+
+  & > h1:not(:first-child) {
+    @apply mt-6;
+  }
+
+  h1 {
+    @apply text-gray-1;
+    @apply text-xl;
+    @apply font-extrabold;
+  }
+
+  a {
+    font-weight: 700;
+    text-decoration: underline;
+    text-decoration-color: var(--gray-5);
+    text-decoration-skip-ink: none;
+    text-decoration-thickness: 2px;
+    text-underline-offset: spacing(0.5);
+  }
+}
+</style>
--- src/views/NoBluetooth.vue
+++ src/views/NoBluetooth.vue
@@ -1,40 +1,18 @@
 <template>
-  <div class="markdown flex flex-col justify-center items-center h-full p-4">
-    <p>
-      The Web Bluetooth API is used to communicate with heart rate monitors and smart trainers. It
-      is currently only supported by
-      <a
-        href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Bluetooth_API#browser_compatibility"
-        target="_blank"
-        >Chromium browsers</a
-      >. Chrome on iOS devices
-      <a
-        href="https://support.google.com/chrome/answer/6362090?hl=en&co=GENIE.Platform%3DiOS"
-        target="_blank"
-        >doesn't support Web Bluetooth</a
-      >. You will need to use an app like
-      <a href="https://apps.apple.com/us/app/bluefy-web-ble-browser/id1492822055" target="_blank"
-        >Bluefy</a
-      >
-      that does.
-    </p>
+  <div class="flex flex-col justify-center items-center h-full p-4">
+    <Markdown class="max-w-screen-sm" :content="content" />
   </div>
 </template>
 
-<style lang="scss" scoped>
-.markdown {
-  p {
-    @apply max-w-screen-sm;
-    @apply leading-relaxed;
-  }
+<script setup>
+import Markdown from '../components/Markdown.vue';
 
-  a {
-    font-weight: 700;
-    text-decoration: underline;
-    text-decoration-color: var(--gray-5);
-    text-decoration-skip-ink: none;
-    text-decoration-thickness: 2px;
-    text-underline-offset: spacing(0.5);
-  }
-}
-</style>
+const content = `
+# No Bluetooth
+
+The Web Bluetooth API is used to communicate with heart rate monitors and smart trainers.
+It is currently only supported by [Chromium browsers](https://developer.mozilla.org/en-US/docs/Web/API/Web_Bluetooth_API#browser_compatibility).
+Chrome on iOS devices [doesn't support Web Bluetooth](https://support.google.com/chrome/answer/6362090?hl=en&co=GENIE.Platform%3DiOS).
+You will need to use an app like [Bluefy](https://apps.apple.com/us/app/bluefy-web-ble-browser/id1492822055) that does.
+`;
+</script>
