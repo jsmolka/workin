0c075bc7e27bb050f8fca807fff4e08054dad71d
jsmolka <jsmolka@pm.me>
2024-01-09 22:11:38 +0100

    improve no bluetooth screen

--- src/views/NoBluetooth.vue
+++ src/views/NoBluetooth.vue
@@ -1,13 +1,40 @@
 <template>
-  <div class="flex justify-center items-center h-full p-4">
+  <div class="markdown flex flex-col justify-center items-center h-full p-4">
     <p>
-      Your browser doesn't support Bluetooth. Click
+      The Web Bluetooth API is used to communicate with heart rate monitors and smart trainers. It
+      is currently only supported by
       <a
-        class="text-blue-3"
         href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Bluetooth_API#browser_compatibility"
-        >here</a
+        target="_blank"
+        >Chromium browsers</a
+      >. Chrome on iOS devices
+      <a
+        href="https://support.google.com/chrome/answer/6362090?hl=en&co=GENIE.Platform%3DiOS"
+        target="_blank"
+        >doesn't support Web Bluetooth</a
+      >. You will need to use an app like
+      <a href="https://apps.apple.com/us/app/bluefy-web-ble-browser/id1492822055" target="_blank"
+        >Bluefy</a
       >
-      to learn more.
+      that does.
     </p>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.markdown {
+  p {
+    @apply max-w-screen-sm;
+    @apply leading-relaxed;
+  }
+
+  a {
+    font-weight: 700;
+    text-decoration: underline;
+    text-decoration-color: var(--gray-5);
+    text-decoration-skip-ink: none;
+    text-decoration-thickness: 2px;
+    text-underline-offset: spacing(0.5);
+  }
+}
+</style>
