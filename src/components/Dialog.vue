8eef3ae0f170113e443d2605e4cb0de7bd0a5d4c
jsmolka <jsmolka@pm.me>
2024-01-21 15:27:02 +0100

    fix dialog not working on mobile

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -10,34 +10,33 @@
         leave-from="opacity-100"
         leave-to="opacity-0"
       >
-        <div
-          class="fixed inset-0 flex justify-center items-center p-4 bg-black/50 z-10"
-          @touchstart.prevent
-        >
-          <TransitionChild
-            as="template"
-            enter="duration-150 ease-out"
-            enter-from="scale-95"
-            enter-to="scale-100"
-            leave="duration-150 ease-in"
-            leave-from="scale-100"
-            leave-to="scale-95"
-          >
-            <DialogPanel class="max-w-screen-sm p-4 bg-shade-7 rounded-sm shadow">
-              <Form>
-                <div v-html="state.content" />
-                <div class="flex justify-end gap-4">
-                  <Button
-                    v-for="button in state.buttons"
-                    @click="close(button.value)"
-                    v-bind="button"
-                  />
-                </div>
-              </Form>
-            </DialogPanel>
-          </TransitionChild>
-        </div>
+        <div class="fixed inset-0 bg-black/50 z-[9998]" />
       </TransitionChild>
+
+      <div class="fixed inset-0 flex justify-center items-center p-4 z-[9999]">
+        <TransitionChild
+          as="template"
+          enter="duration-150 ease-out"
+          enter-from="opacity-0 scale-95"
+          enter-to="opacity-100 scale-100"
+          leave="duration-150 ease-in"
+          leave-from="opacity-100 scale-100"
+          leave-to="opacity-0 scale-95"
+        >
+          <DialogPanel class="max-w-screen-sm p-4 bg-shade-7 rounded-sm shadow z-20">
+            <Form>
+              <div v-html="state.content" />
+              <div class="flex justify-end gap-4">
+                <Button
+                  v-for="button in state.buttons"
+                  @click="close(button.value)"
+                  v-bind="button"
+                />
+              </div>
+            </Form>
+          </DialogPanel>
+        </TransitionChild>
+      </div>
     </Dialog>
   </TransitionRoot>
 </template>
