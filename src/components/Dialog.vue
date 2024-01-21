93175aa583fca0a8c703a9ec7516c8b3c1b8546c
jsmolka <jsmolka@pm.me>
2024-01-21 14:29:47 +0100

    animate dialog

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,45 +1,71 @@
 <template>
-  <Dialog :open="data.open" @close="close" style="touch-action: none">
-    <div
-      class="fixed inset-0 flex justify-center items-center p-4 bg-black/50 z-[9999]"
-      @touchstart.prevent
-    >
-      <DialogPanel class="max-w-screen-sm p-4 bg-shade-7 rounded-sm shadow">
-        <Form>
-          <div v-html="data.content" />
-          <div class="flex justify-end gap-4">
-            <Button v-for="button in data.buttons" @click="close(button.value)" v-bind="button" />
-          </div>
-        </Form>
-      </DialogPanel>
-    </div>
-  </Dialog>
+  <TransitionRoot appear :show="state.visible" as="template">
+    <Dialog :open="state.visible" @close="close">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-300 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div
+          class="fixed inset-0 flex justify-center items-center p-4 bg-black/50 z-10"
+          @touchstart.prevent
+        >
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="scale-95"
+            enter-to="scale-100"
+            leave="duration-300 ease-in"
+            leave-from="scale-100"
+            leave-to="scale-95"
+          >
+            <DialogPanel class="max-w-screen-sm p-4 bg-shade-7 rounded-sm shadow">
+              <Form>
+                <div v-html="state.content" />
+                <div class="flex justify-end gap-4">
+                  <Button
+                    v-for="button in state.buttons"
+                    @click="close(button.value)"
+                    v-bind="button"
+                  />
+                </div>
+              </Form>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </TransitionChild>
+    </Dialog>
+  </TransitionRoot>
 </template>
 
 <script setup>
-import { Dialog, DialogPanel } from '@headlessui/vue';
+import { Dialog, DialogPanel, TransitionChild, TransitionRoot } from '@headlessui/vue';
 import { reactive } from 'vue';
 import Button from './Button.vue';
 import Form from './Form.vue';
 
-const data = reactive({
+const state = reactive({
   content: '',
   buttons: [],
   resolve: null,
-  open: false,
+  visible: false,
 });
 
 const show = async (content, buttons) =>
   new Promise((resolve) => {
-    data.content = content;
-    data.buttons = buttons;
-    data.resolve = resolve;
-    data.open = true;
+    state.content = content;
+    state.buttons = buttons;
+    state.resolve = resolve;
+    state.visible = true;
   });
 
 const close = (value = null) => {
-  data.open = false;
-  data.resolve(value);
+  state.visible = false;
+  state.resolve(value);
 };
 
 defineExpose({ show });
