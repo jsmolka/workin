bb41a8de33f42103f7236e28a986083674e1508b
jsmolka <jsmolka@pm.me>
2024-06-09 11:23:21 +0200

    use new components in settings

--- src/views/settings/Index.vue
+++ src/views/settings/Index.vue
@@ -1,6 +1,7 @@
 <template>
   <Form>
-    <Label text="Smart trainer">
+    <FormItem>
+      <Label>Smart trainer</Label>
       <DeviceButton
         :device="trainer"
         @update:device="setTrainer"
@@ -9,45 +10,53 @@
       >
         {{ `${device.name} [${device.powerRange.min} - ${device.powerRange.max} W]` }}
       </DeviceButton>
-    </Label>
-    <Label text="Heart rate monitor">
+    </FormItem>
+
+    <FormItem>
+      <Label>Heart rate monitor</Label>
       <DeviceButton v-model:device="hrm" :constructor="HeartRate" v-slot="{ device }">
         {{ device.heartRate != null ? `${device.name} [${device.heartRate} bpm]` : device.name }}
       </DeviceButton>
-    </Label>
-    <Label text="FTP [W]">
-      <InputNumber :min="1" v-model:value="athlete.ftp" />
-    </Label>
-    <Label text="Log level">
-      <Select :items="log.levels" v-model:value="settings.logLevel">
+    </FormItem>
+
+    <FormItem>
+      <Label>FTP</Label>
+      <InputNumber v-model="athlete.ftp" :min="1" :max="500" suffix=" W" />
+    </FormItem>
+
+    <FormItem>
+      <Label>Log level</Label>
+      <Select :items="log.levels" v-model="settings.logLevel">
         <template #item="{ item }">
-          <span class="capitalize">
-            {{ item }}
-          </span>
+          <SelectItemText>
+            <span class="capitalize">{{ item }}</span>
+          </SelectItemText>
         </template>
       </Select>
-    </Label>
-    <Label text="Log as notification">
-      <Switch v-model:value="settings.logAsNotification" />
-    </Label>
+    </FormItem>
+
+    <FormItem>
+      <Label>Log as notification</Label>
+      <Switch v-model="settings.logAsNotification" />
+    </FormItem>
   </Form>
 </template>
 
 <script setup>
+import { Form, FormItem } from '@/components/ui/form';
+import { InputNumber } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Select, SelectItemText } from '@/components/ui/select';
+import { Switch } from '@/components/ui/switch';
+import { FitnessMachine } from '@/modules/bluetooth/fitnessMachine';
+import { HeartRate } from '@/modules/bluetooth/heartRate';
+import { useAthleteStore } from '@/stores/athlete';
+import { useDevicesStore } from '@/stores/devices';
+import { useSettingsStore } from '@/stores/settings';
+import { log } from '@/utils/log';
+import { toast } from '@/utils/toast';
+import DeviceButton from '@/views/settings/DeviceButton.vue';
 import { storeToRefs } from 'pinia';
-import Form from '../../components/Form.vue';
-import InputNumber from '../../components/InputNumber.vue';
-import Label from '../../components/Label.vue';
-import Select from '../../components/Select.vue';
-import Switch from '../../components/Switch.vue';
-import { FitnessMachine } from '../../modules/bluetooth/fitnessMachine';
-import { HeartRate } from '../../modules/bluetooth/heartRate';
-import { useAthleteStore } from '../../stores/athlete';
-import { useDevicesStore } from '../../stores/devices';
-import { useSettingsStore } from '../../stores/settings';
-import { log } from '../../utils/log';
-import { toast } from '../../utils/toast';
-import DeviceButton from './DeviceButton.vue';
 
 const { athlete } = storeToRefs(useAthleteStore());
 const { hrm, trainer } = storeToRefs(useDevicesStore());
@@ -58,7 +67,7 @@ const setTrainer = (device) => {
     return;
   }
   if (!device.supportsPower) {
-    toast.info(`${device.name} does not support power`);
+    toast(`${device.name} does not support power`);
     device.disconnect();
     return;
   }
