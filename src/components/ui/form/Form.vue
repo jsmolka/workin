5112b11536958655baa72b0c48d757867db7a6e2
jsmolka <jsmolka@pm.me>
2024-06-14 11:50:44 +0200

    update form

--- src/components/ui/form/Form.vue
+++ src/components/ui/form/Form.vue
@@ -1,13 +1,16 @@
 <template>
-  <div :class="cn('flex flex-col gap-4', props.class)">
+  <Primitive :as="as" :as-child="asChild" :class="cn('flex flex-col gap-4', props.class)">
     <slot />
-  </div>
+  </Primitive>
 </template>
 
 <script setup>
 import { cn } from '@/utils/ui';
+import { Primitive } from 'radix-vue';
 
 const props = defineProps({
+  as: { type: String, default: 'div' },
+  asChild: { type: Boolean, required: false },
   class: { required: false },
 });
 </script>
