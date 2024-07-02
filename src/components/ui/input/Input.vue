71f4cd9941ce940480c8a4cfa67027ba94e0ac26
jsmolka <jsmolka@pm.me>
2024-07-02 21:12:13 +0200

    update components

--- src/components/ui/input/Input.vue
+++ src/components/ui/input/Input.vue
@@ -2,7 +2,7 @@
   <input
     :class="
       cn(
-        'flex w-full h-8 px-2 py-1.5 bg-shade-7 border rounded-sm placeholder:text-shade-3 disabled:opacity-50 disabled:cursor-not-allowed',
+        'flex w-full h-8 px-2 py-1.5 bg-shade-7 border rounded-sm overflow-ellipsis placeholder:text-shade-3 disabled:opacity-50 disabled:cursor-not-allowed',
         props.class,
       )
     "
@@ -19,6 +19,5 @@ const modelValue = defineModel({ type: String, required: false });
 
 const props = defineProps({
   class: { required: false },
-  defaultValue: { type: [String, Number], required: false },
 });
 </script>
--- src/components/ui/input/InputNumber.vue
+++ src/components/ui/input/InputNumber.vue
@@ -2,7 +2,7 @@
   <input
     :class="
       cn(
-        'flex w-full h-8 px-2 py-1.5 bg-shade-7 border rounded-sm placeholder:text-shade-3 disabled:opacity-50 disabled:cursor-not-allowed',
+        'flex w-full h-8 px-2 py-1.5 bg-shade-7 border rounded-sm overflow-ellipsis placeholder:text-shade-3 disabled:opacity-50 disabled:cursor-not-allowed',
         props.class,
       )
     "
