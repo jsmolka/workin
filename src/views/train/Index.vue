6b65e98c3fbc45374e9e6c828fd693712be0b7c5
jsmolka <jsmolka@pm.me>
2024-07-07 13:49:52 +0200

    remove no workout component

--- src/views/train/Index.vue
+++ src/views/train/Index.vue
@@ -1,13 +1,18 @@
 <template>
   <Train v-if="activity" />
-  <NoWorkout v-else />
+  <div v-else class="flex flex-col justify-center items-center gap-2 p-4">
+    <p>Select a workout to start training.</p>
+    <Button @click="router.push('/workouts')">Workouts</Button>
+  </div>
 </template>
 
 <script setup>
+import { Button } from '@/components/ui/button';
 import { useActivityStore } from '@/stores/activity';
-import NoWorkout from '@/views/train/NoWorkout.vue';
 import Train from '@/views/train/Train.vue';
 import { storeToRefs } from 'pinia';
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
 const { activity } = storeToRefs(useActivityStore());
 </script>
