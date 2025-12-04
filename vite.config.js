a60cf7c18e5c0e4dd1116e1a370692d3561b75c9
jsmolka <jsmolka@pm.me>
2025-12-04 20:48:38 +0100

    add pwa support

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,40 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
-
-function relative(path) {
-  return fileURLToPath(new URL(path, import.meta.url));
-}
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [vue(), vueJsx(), tailwindcss()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'workin',
+        description: 'A web app to create and run smart trainer workouts.',
+        theme_color: '#242933',
+        background_color: '#242933',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      '@': relative('./src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   build: {
