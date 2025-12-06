e139329868a759e65ecfaa53504ff0240d6c9668
jsmolka <jsmolka@pm.me>
2025-12-06 17:58:14 +0100

    refactor

--- src/components/Laps.vue
+++ src/components/Laps.vue
@@ -3,7 +3,7 @@
     <DataTable ref="table" class="font-feature-tnum absolute inset-0">
       <DataTableBody>
         <DataTableRow
-          v-for="(dataPoints, index) in items"
+          v-for="(records, index) in items"
           class="grid auto-cols-fr grid-flow-col"
           :index="index"
           v-model:selected-index="selectedIndex"
@@ -11,19 +11,19 @@
           <DataTableCell>
             <div class="flex justify-start">
               <Reserve class="text-right" reserve="100 W">
-                {{ Math.round(dataPoints.averagePower()) }} W
+                {{ Math.round(records.averagePower()) }} W
               </Reserve>
             </div>
           </DataTableCell>
-          <DataTableCell v-if="hasHeartRate(dataPoints)">
+          <DataTableCell v-if="hasHeartRate(records)">
             <div class="flex justify-center">
               <Reserve class="text-right" reserve="100 bpm">
-                {{ Math.round(dataPoints.averageHeartRate()) }} bpm
+                {{ Math.round(records.averageHeartRate()) }} bpm
               </Reserve>
             </div>
           </DataTableCell>
           <DataTableCell class="text-right">
-            {{ formatSeconds(dataPoints.length) }}
+            {{ formatSeconds(records.length) }}
           </DataTableCell>
         </DataTableRow>
       </DataTableBody>
@@ -49,8 +49,8 @@ defineProps({
   items: { type: Array, required: true },
 });
 
-const hasHeartRate = (dataPoints) => {
-  return dataPoints.some((dataPoint) => dataPoint.heartRate != null);
+const hasHeartRate = (records) => {
+  return records.some((record) => record.heartRate != null);
 };
 
 const table = useTemplateRef('table');
